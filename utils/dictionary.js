ead39d1071266fd50bc53ae8bf1b55fc2163fd3c
xiaojingming <2413294257@qq.com>
2022-06-13 17:07:03 +0800

    address change

--- utils/dictionary.js
+++ utils/dictionary.js
@@ -1,11 +1,11 @@
 const dictionary = [
   {
     name: 'vue2模板',
-    value: 'github:xiaojingming/my-cli'
-  },
-  {
-    name: 'vue3模板',
-    value: 'github:xiaojingming/my-cli'
+    value: 'github:xiaojingming/panel-template'
   }
+  // {
+  //   name: 'vue3模板',
+  //   value: 'github:xiaojingming/my-cli'
+  // }
 ];
 module.exports = dictionary;
